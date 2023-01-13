22fdd0c90400171ca9f419f77f05056bbc11b3c6
Chris Bouma <chr1sje.bouma@gmail.com>
2023-01-13 11:28:37 +0100

    Initial commit

--- db/database.sql
+++ db/database.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS `php-pdo-crud-toets`;
+CREATE DATABASE IF NOT EXISTS `php-pdo-crud-toets`DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `php-pdo-crud-toets`;
 
 DROP TABLE IF EXISTS `DureAuto`;
@@ -8,4 +8,12 @@ CREATE TABLE IF NOT EXISTS `DureAuto` (
   `Model` varchar(200),
   `Topsnelheid` smallint(4),
   `Prijs` varchar(200),
-  PRIMARY KEY = `id`, ENGINE=InnoDB;
+ PRIMARY KEY (`Id`)
+) ENGINE=InnoDB;
+
+insert into DureAuto(`Merk`,`Model`,`Topsnelheid`,`Prijs`) values 
+    ('Bugatti','La voiture noire',420,'16500000'),
+    ('Rolls-Royce','Swaptail',250,'10960000'),
+    ('Bugatti','EB110',390,'7500000'),
+    ('Mercedes-Benz','Maybach Exelero',350,'6700000'),
+    ('Koenigsegg','CCXR Trevita',401,'4000000')
