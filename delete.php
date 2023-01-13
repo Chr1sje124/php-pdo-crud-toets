79bf190914833177c5ae5853b4b60a394b268b81
Chris Bouma <chr1sje.bouma@gmail.com>
2023-01-13 11:58:06 +0100

    Initial commit

--- delete.php
+++ delete.php
@@ -12,3 +12,19 @@ try {
 } catch(PDOException $e) {
     $e->getMessage();
 }
+
+$sql = "DELETE FROM DureAuto WHERE Id = :Id";
+
+$statement = $pdo->prepare($sql);
+
+$statement->bindValue(':Id', $_GET['Id'], PDO::PARAM_INT);
+
+$result = $statement->execute();
+
+if ($result) {
+    echo "Het record is succesvol verwijderd";
+    header('Refresh:1; url=read.php');
+} else {
+    echo "Het record is niet verwijderd";
+    header('Refresh:1; url=read.php');
+}
